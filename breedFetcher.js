86b94b9fac72b4022322214e563beabf5d54adb5
Zoey Farago <zf.zoodle@gmail.com>
2021-01-14 20:41:56 +0000

    Refactoring with callback function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,10 @@
 const request = require("request");
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
-const breedInput = () => {
-  rl.question('Please enter a cat breed: ', (breed) => {
-    fetchBreed(breed);
-  });
-};
-
-const fetchBreed = (breed) => {
+const fetchBreedDescription = (breed, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error,response,body) => {
-    if (error) {
-      console.log('error: ', error);
-      process.exit();
-    }
     const data = JSON.parse(body);
-    (data[0]) ? console.log(data[0].description) : console.log("Breed not found :(");
-    process.exit();
+    (data[0]) ? callback(null, data[0].description) : callback(error, 'Cat breed not found :(');
   });
 };
 
-breedInput();
-
+module.exports = fetchBreedDescription;
